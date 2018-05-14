e08ff4b3a64fca53046c94499ae4be7dc3e94b77
alexlo0612 <alexlo0612@hotmail.com.tw>
2018-05-14 23:06:47 +0800

    fix permission

--- AlgoBot1/app.js
+++ AlgoBot1/app.js
@@ -1,45 +1,45 @@
 //Require Dependencies
-const discord = require("discord.js");
+const discord = require('discord.js');
 //Create Bot
 const client = new discord.Client();
 //Global Variable -> Bot Token
-const bot_token = "NDQ0ODk3NDQzNTA3NjAxNDIx.DdinfQ.wxeo8_dc08vtBrCiaK88dwMdeLI";
+const bot_token = 'Myth';
 
 //Help Command (showing startsWith element)
-client.on("message", message => {
+client.on('message', message => {
   if (message.author.bot) return;
-  let prefix = "!";
-  if (message.content.startsWith(prefix) && message.content === "!help") {
+  let prefix = '!';
+  if (message.content.startsWith(prefix) && message.content === '!help') {
     message.channel.send(
-      "Public Command:[ dm , avt , whats my id , whats this server , Am I the Admin ]"
+      'Public Command:[ dm , avt , whats my id , whats this server , am i the admin ]'
     );
     message.channel.send(
-      "-------------------------------------------------------------------------------------------"
+      '-------------------------------------------------------------------------------------------'
     );
-    message.channel.send("Admin Commnad:[ ping ]");
+    message.channel.send('Admin Commnad:[ ping ]');
   }
 });
 
 //Return the Channel name of the sender
-client.on("message", message => {
-  if (message.content === "whats this server") {
+client.on('message', message => {
+  if (message.content === 'whats this server') {
     message.channel.send(message.guild.name);
   }
 });
 
 //Return the username of the sender
-client.on("message", message => {
-  if (message.content === "whats my id") {
+client.on('message', message => {
+  if (message.content === 'whats my id') {
     message.channel.send(message.member.user.username);
   }
 });
 
 //Check if the sender of the message if the @Owner
-client.on("message", message => {
-  if (message.content === "Am I the Admin") {
+client.on('message', message => {
+  if (message.content === 'am i the admin') {
     message.channel.send(
       message.member.roles.some(find =>
-        ["@Owner", "@Programmer", "@Back-End Dev.", "@Front-End Dev."].includes(
+        ['Owner', 'Programmer', 'Back-End Dev.', 'Front-End Dev.'].includes(
           find.name
         )
       )
@@ -47,36 +47,36 @@ client.on("message", message => {
   }
 });
 //DM the sender of the message
-client.on("message", message => {
-  if (message.content === "dm") {
-    message.author.send("You said ping!");
+client.on('message', message => {
+  if (message.content === 'dm') {
+    message.author.send('You said ping!');
   }
 });
 //Retrieve the Avartar URL of the sender
-client.on("message", message => {
-  if (message.content === "avt") {
+client.on('message', message => {
+  if (message.content === 'avt') {
     message.reply(message.author.avatarURL);
   }
 });
 //Respond to Ping only if the sender is the @Owner
-client.on("message", message => {
+client.on('message', message => {
   if (
-    message.content === "ping" &&
+    message.content === 'ping' &&
     message.member.roles.some(
       role =>
-        ["@Owner", "@Programmer", "@Back-End Dev.", "@Front-End Dev."].includes(
+        ['Owner', 'Programmer', 'Back-End Dev.', 'Front-End Dev.'].includes(
           role.name
         ) == true
     )
   ) {
-    message.channel.send("Pong!");
+    message.channel.send('Pong!');
   }
 });
 
 //Server Greeting!
-client.on("guildMemberAdd", member => {
+client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find("name", "member-log");
+  const channel = member.guild.channels.find('name', 'member-log');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
--- AlgoBot2/app.js
+++ AlgoBot2/app.js
@@ -1,10 +1,91 @@
 //Require Dependencies
-const discord = require("discord.js");
-const fs = require("fs");
+const discord = require('discord.js');
+const fs = require('fs');
 //Config Files
-const login_config = require("./config.json");
+const login_config = require('./config.json');
 //Initialize Bot
 const client = new discord.Client();
+
+//Help Command (showing startsWith element)
+client.on('message', message => {
+  if (message.author.bot) return;
+  let prefix = '!';
+  if (message.content.startsWith(prefix) && message.content === '!help') {
+    message.channel.send(
+      'Public Command:[ dm , avt , whats my id , whats this server , am i the admin ]'
+    );
+    message.channel.send(
+      '-------------------------------------------------------------------------------------------'
+    );
+    message.channel.send('Admin Commnad:[ ping ]');
+  }
+});
+
+//Return the Channel name of the sender
+client.on('message', message => {
+  if (message.content === 'whats this server') {
+    message.channel.send(message.guild.name);
+  }
+});
+
+//Return the username of the sender
+client.on('message', message => {
+  if (message.content === 'whats my id') {
+    message.channel.send(message.member.user.username);
+  }
+});
+
+//Check if the sender of the message if the @Owner
+client.on('message', message => {
+  if (message.content === 'am i the admin') {
+    message.channel.send(
+      message.member.roles.some(find =>
+        ['Owner', 'Programmer', 'Back-End Dev.', 'Front-End Dev.'].includes(
+          find.name
+        )
+      )
+    );
+  }
+});
+//DM the sender of the message
+client.on('message', message => {
+  if (message.content === 'dm') {
+    message.author.send("'Yo! What'Up!");
+  }
+});
+//Retrieve the Avartar URL of the sender
+client.on('message', message => {
+  if (message.content === 'avt') {
+    message.reply(message.author.avatarURL);
+  }
+});
+//Respond to Ping only if the sender is Mod or ME!
+client.on('message', message => {
+  if (
+    message.author.username === 'alexlo0612' ||
+    (message.content === 'ping' &&
+      message.member.roles.some(
+        role =>
+          ['Owner', 'Programmer', 'Back-End Dev.', 'Front-End Dev.'].includes(
+            role.name
+          ) == true
+      ))
+  ) {
+    message.channel.send('Pong!');
+  }
+});
+
+//Server Greeting!
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(
+    `Greeting from team The Algorithm: Welcome to the server, ${member}`
+  );
+});
 
 //Auth Bot
 client.login(login_config.token);
